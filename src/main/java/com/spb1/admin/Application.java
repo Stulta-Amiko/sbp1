b91a0ab20475a520d325f3c3692c5e9928bc5047
Stulta-Amiko <phh01010312@gmail.com>
2023-02-17 14:02:42 +0900

    테스트 코드작성 그레들 수정 lombok 의존성 추가

---
+++ src/main/java/com/spb1/admin/Application.java
@@ -0,0 +1,11 @@
+package com.spb1.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application{
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/spb1/admin/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.spb1.admin.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
