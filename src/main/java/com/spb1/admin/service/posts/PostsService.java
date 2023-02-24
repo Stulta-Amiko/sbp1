8d826ea204847701b5c74e6a8766b6c6eb0997bb
Stulta-Amiko <phh01010312@gmail.com>
2023-02-24 18:36:14 +0900

    게시글 수정 삭제기능 추가 23/02/24

--- src/main/java/com/spb1/admin/service/posts/PostsService.java
+++ src/main/java/com/spb1/admin/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.spb1.admin.service.posts;
 
 import com.spb1.admin.domain.posts.Posts;
 import com.spb1.admin.domain.posts.PostsRepository;
+import com.spb1.admin.web.dto.PostsListResponseDto;
 import com.spb1.admin.web.dto.PostsResponseDto;
 import com.spb1.admin.web.dto.PostsSaveRequestDto;
 import com.spb1.admin.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -25,6 +29,17 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 
     public PostsResponseDto findById (Long id) {
